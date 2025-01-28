1b0471f559bd9f79fe4cc64ea27b619442fdab03
JetLuck <n.korogodin@g.nsu.ru>
2025-01-28 22:32:47 +0700

    correct

--- LeetCodeTasks.cpp
+++ LeetCodeTasks.cpp
@@ -20,10 +20,15 @@ public:
         {
 	        for (int j = 0; j < grid[0].size(); j++)
 	        {
-		        if (grid[i][j] == 0 || visited[i][j] == 1)
+		        if (grid[i][j] == 0)
 		        {
+                    visited[i][j] = 1;
 			        continue;
 		        }
+                if (visited[i][j] == 1)
+                {
+                    continue;
+                }
                 queue<pair<int,int>> queue;
                 queue.emplace(i, j);
                 int fishes = 0;
@@ -36,17 +41,23 @@ public:
 	                    continue;
                     }
                     fishes += grid[point.first][point.second];
+                    visited[point.first][point.second] = 1;
                     for (int k = 0; k < 4; k++)
                     {
-	                    if (point.first + dy[i] > grid.size()-1 || point.first + dy[i] < 0 || point.second + dx[i] > grid[0].size() - 1 || point.second + dx[i] < 0)
+	                    if (point.first + dy[k] > grid.size()-1 || point.first + dy[k] < 0 || point.second + dx[k] > grid[0].size() - 1 || point.second + dx[k] < 0)
 	                    {
 		                    continue;
 	                    }
-                        if (visited[point.first+dy[i]][point.second+dx[i]])
+                        if (grid[point.first + dy[k]][point.second + dx[k]] == 0)
+                        {
+                            visited[point.first + dy[k]][point.second + dx[k]] = 1;
+                            continue;
+                        }
+                        if (visited[point.first+dy[k]][point.second+dx[k]])
                         {
 	                        continue;
                         }
-                        queue.emplace(point.first + dy[i], point.second + dx[i]);
+                        queue.emplace(point.first + dy[k], point.second + dx[k]);
                     }
                 }
                 if (fishes > max_fishes)
@@ -63,7 +74,6 @@ int main()
 {
     Solution sol;
     std::string input{ "[[0,2,1,0],[4,0,0,3],[1,0,0,4],[0,3,2,0]]" };
-    std::vector<int> grid = Parser::process_vector(input);
-    long long res = sol.countServers(grid);
-    std::cout << res;
+    std::vector<std::vector<int>> grid = Parser::process_vector_vector(input);
+    long long res = sol.findMaxFish(grid);
 }
--- parser.cpp
+++ parser.cpp
@@ -23,5 +23,36 @@ std::vector<int> Parser::process_vector(std::string& input)
 
 std::vector<std::vector<int>> Parser::process_vector_vector(std::string& input)
 {
-	
+    std::vector<std::vector<int>> res;
+    std::vector<int> line;
+    int i = 2;
+    line.push_back(atoi(&input[2]));
+    while (i < input.size() - 1)
+    {
+        while (input[i] != ',' && input[i] != ']' && input[i] != '[')
+        {
+            i++;
+        }
+        if (input[i] == '[')
+        {
+            line.push_back(atoi(&input[i + 1]));
+        }
+        else if (input[i] == ',')
+        {
+            if (input[i + 1] == '[')
+            {
+                i++;
+                continue;
+            }
+            line.push_back(atoi(&input[i + 1]));
+        }
+        else
+        {
+            res.push_back(line);
+            line.clear();
+
+        }
+        i++;
+    }
+    return res;
 }
