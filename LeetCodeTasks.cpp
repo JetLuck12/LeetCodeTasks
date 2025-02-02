3aed274c67b5a38a341e1185a36de4732328e04f
JetLuck <n.korogodin@g.nsu.ru>
2025-02-02 14:13:03 +0700

    correct

--- LeetCodeTasks.cpp
+++ LeetCodeTasks.cpp
@@ -11,22 +11,43 @@ using namespace std;
 
 class Solution {
 public:
-    bool isArraySpecial(vector<int>& nums) {
-        for (int i = 0; i < nums.size()-1; i++)
+    bool check(vector<int>& nums) {
+        int max_ind = 0;
+        for (max_ind = 0; max_ind < nums.size() - 1; max_ind++)
         {
-	        if (((nums[i]+nums[i+1])&1) == 0)
-	        {
-                return false;
-	        }
+            if (nums[max_ind] > nums[max_ind + 1])
+            {
+                break;
+            }
         }
-        return true;
+        if (max_ind == nums.size() - 1)
+        {
+            return true;
+        }
+        if (nums.front() < nums.back())
+        {
+            return false;
+        }
+        int min_ind;
+        for (min_ind = nums.size() - 1; min_ind >= max_ind; min_ind--)
+        {
+            if (nums[min_ind] < nums[min_ind - 1])
+            {
+                break;
+            }
+        }
+        if (min_ind - max_ind == 1)
+        {
+            return true;
+        }
+        return false;
     }
 };
 
 int main()
 {
     Solution sol;
-    std::string input{ "[2,1,4]" };
+    std::string input{ "[1,2,3]" };
     std::vector<int> grid = Parser::process_vector(input);
-    bool res = sol.isArraySpecial(grid);
+    bool res = sol.check(grid);
 }
