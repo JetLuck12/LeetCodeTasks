e22d561d53844c4a6f857c0821b483c94c7cb428
JetLuck <n.korogodin@g.nsu.ru>
2025-02-03 13:10:35 +0700

    correct

--- LeetCodeTasks.cpp
+++ LeetCodeTasks.cpp
@@ -11,13 +11,34 @@ using namespace std;
 
 class Solution {
 public:
-
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int max_size = 1;
+        int dec_size = 1;
+        int inc_size = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i - 1]) {
+                dec_size++;
+                inc_size = 1;
+                max_size = max(dec_size, max_size);
+            }
+            else if (nums[i] > nums[i - 1]) {
+                inc_size++;
+                dec_size = 1;
+                max_size = max(inc_size, max_size);
+            }
+            else {
+                inc_size = 1;
+                dec_size = 1;
+            }
+        }
+        return max_size;
+    }
 };
 
 int main()
 {
     Solution sol;
-    std::string input{ "[[0,2,1,0],[4,0,0,3],[1,0,0,4],[0,3,2,0]]" };
-    std::vector<std::vector<int>> grid = Parser::process_vector_vector(input);
-    //long long res = sol.findMaxFish(grid);
+    std::string input{ "[3,3,3,3]" };
+    std::vector<int> grid = Parser::process_vector(input);
+    int res = sol.longestMonotonicSubarray(grid);
 }
