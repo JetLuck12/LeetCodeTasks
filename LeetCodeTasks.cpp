c65c9e90f87fc7d3d0fc5e8f26090a4763b0e80e
JetLuck <n.korogodin@g.nsu.ru>
2025-02-04 10:38:51 +0700

    correct

--- LeetCodeTasks.cpp
+++ LeetCodeTasks.cpp
@@ -11,13 +11,34 @@ using namespace std;
 
 class Solution {
 public:
-
+    int maxAscendingSum(vector<int>& nums) {
+        int max_sum = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.size()-1; i++)
+        {
+            sum += nums[i];
+            if (nums[i] >= nums[i+1])
+            {
+                if (sum > max_sum)
+                {
+                    max_sum = sum;
+                }
+                sum = 0;
+            }
+        }
+        sum += nums[nums.size()-1];
+        if (sum > max_sum)
+        {
+            max_sum = sum;
+        }
+        return max_sum;
+    }
 };
 
 int main()
 {
     Solution sol;
-    std::string input{ "[[0,2,1,0],[4,0,0,3],[1,0,0,4],[0,3,2,0]]" };
-    std::vector<std::vector<int>> grid = Parser::process_vector_vector(input);
-    //long long res = sol.findMaxFish(grid);
+    std::string input{ "[10,20,30,5,10,50]" };
+    std::vector<int> grid = Parser::process_vector(input);
+    int res = sol.maxAscendingSum(grid);
 }
