bb12b53349f3957e189460ea2ce414cba472f0ed
JetLuck <n.korogodin@g.nsu.ru>
2025-02-09 16:51:08 +0700

    correct

--- LeetCodeTasks.cpp
+++ LeetCodeTasks.cpp
@@ -11,13 +11,35 @@ using namespace std;
 
 class Solution {
 public:
-
+    long long countBadPairs(vector<int>& nums) {
+        map<int, int> subs;
+        long long res = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int sub = i - nums[i];
+	        if (subs.contains(sub))
+	        {
+                subs[sub]++;
+	        }
+            else
+            {
+                subs[sub] = 1;
+            }
+        }
+        int left = nums.size();
+        for (auto& [sub, count] : subs)
+        {
+            left -= count;
+            res += count * left;
+        }
+        return res;
+    }
 };
 
 int main()
 {
     Solution sol;
-    std::string input{ "[[0,2,1,0],[4,0,0,3],[1,0,0,4],[0,3,2,0]]" };
-    std::vector<std::vector<int>> grid = Parser::process_vector_vector(input);
-    //long long res = sol.findMaxFish(grid);
+    std::string input{ "[4,1,3,3]" };
+    std::vector<int> grid = Parser::process_vector(input);
+    long long res = sol.countBadPairs(grid);
 }
