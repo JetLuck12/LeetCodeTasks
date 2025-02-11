ecc44df39b079a1df753f0b99017f8b4cf028cea
JetLuck <n.korogodin@g.nsu.ru>
2025-02-11 11:02:16 +0700

    correct

--- LeetCodeTasks.cpp
+++ LeetCodeTasks.cpp
@@ -11,13 +11,21 @@ using namespace std;
 
 class Solution {
 public:
-
+    string removeOccurrences(string s, string part) {
+        size_t pos = s.find(part);
+        while (pos != s.npos)
+        {
+            s.erase(pos, part.size());
+            pos = s.find(part);
+        }
+        return s;
+    }
 };
 
 int main()
 {
     Solution sol;
-    std::string input{ "[[0,2,1,0],[4,0,0,3],[1,0,0,4],[0,3,2,0]]" };
-    std::vector<std::vector<int>> grid = Parser::process_vector_vector(input);
-    //long long res = sol.findMaxFish(grid);
+    std::string input1{ "daabcbaabcbc" };
+    std::string input2{ "abc" };
+    std::string res = sol.removeOccurrences(input1, input2);
 }
