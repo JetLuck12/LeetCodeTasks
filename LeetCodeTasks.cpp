5e3ed8fcda6ce2968775cdc2269324aad12b5932
JetLuck <n.korogodin@g.nsu.ru>
2025-02-13 13:41:08 +0700

    correct

--- LeetCodeTasks.cpp
+++ LeetCodeTasks.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <map>
 #include <queue>
+#include <set>
 #include <vector>
 
 #include "parser.h"
@@ -11,13 +12,31 @@ using namespace std;
 
 class Solution {
 public:
-
+    int minOperations(vector<int>& nums, int k) {
+        multiset<long long> set_nums(nums.begin(), nums.end());
+        int i = 0;
+        while (set_nums.size() > 1)
+        {
+            long long a = *set_nums.begin();
+            set_nums.erase(set_nums.begin());
+            if (a >= k)
+            {
+                return i;
+            }
+            long long b = *set_nums.begin();
+            set_nums.erase(set_nums.begin());
+            set_nums.insert(a * 2 + b);
+            i++;
+        }
+        return i;
+    }
 };
 
 int main()
 {
     Solution sol;
-    std::string input{ "[[0,2,1,0],[4,0,0,3],[1,0,0,4],[0,3,2,0]]" };
-    std::vector<std::vector<int>> grid = Parser::process_vector_vector(input);
-    //long long res = sol.findMaxFish(grid);
-}
+    std::string input{ "[1,1,2,4,9]" };
+    int k = 20;
+    std::vector<int> grid = Parser::process_vector(input);
+    int res = sol.minOperations(grid, k);
+	}
