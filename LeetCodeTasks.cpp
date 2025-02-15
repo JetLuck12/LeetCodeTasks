ce76e095c83ba7e9b0e7294e3325f7b3c31a6181
JetLuck <n.korogodin@g.nsu.ru>
2025-02-15 11:51:00 +0700

    correct(7ms)

--- LeetCodeTasks.cpp
+++ LeetCodeTasks.cpp
@@ -9,15 +9,51 @@
 
 using namespace std;
 
+int recursive(int num, int pred = 0, int next = 0) {
+    int temp = next;
+    int diver = 10;
+    while (temp/diver != 0)
+    {
+        int rem = temp % diver;
+        int div_res = temp / diver;
+        if (rem + div_res == num - pred)
+        {
+            return num * num;
+        }
+        if (rem+pred > num)
+        {
+            return 0;
+        }
+        int res = recursive(num, rem + pred, div_res);
+        if (res > 0)
+        {
+            return res;
+        }
+        diver *= 10;
+    }
+    return 0;
+}
+
 class Solution {
 public:
-
+    int punishmentNumber(int n) {
+        int sum = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            int square = i * i;
+            if (square / 10 == 0)
+            {
+                continue;
+            }
+            sum += recursive(i, 0, square);
+        }
+        return sum;
+    }
 };
 
 int main()
 {
     Solution sol;
-    std::string input{ "[[0,2,1,0],[4,0,0,3],[1,0,0,4],[0,3,2,0]]" };
-    std::vector<std::vector<int>> grid = Parser::process_vector_vector(input);
-    //long long res = sol.findMaxFish(grid);
+    int number = 10;
+    int res = sol.punishmentNumber(number);
 }
