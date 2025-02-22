597b3abad6e9aa58783c0cadd71033da572c7afd
JetLuck <n.korogodin@g.nsu.ru>
2025-02-23 02:24:53 +0700

    correct(2ms)

--- LeetCodeTasks.cpp
+++ LeetCodeTasks.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <map>
 #include <queue>
+#include <string>
 #include <vector>
 
 #include "parser.h"
@@ -9,15 +10,59 @@
 
 using namespace std;
 
+ struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+    
+};
 class Solution {
 public:
-
+    TreeNode* recoverFromPreorder(string traversal) {
+        vector<TreeNode*> chain;
+        int dash_count = 0;
+        int temp_val = 0;
+        for (int i = 0; i < traversal.size(); i++) {
+            if (traversal[i] == '-') {
+                dash_count++;
+            }
+            else {
+                if (i != traversal.size() - 1 && traversal[i + 1] != '-') {
+                    temp_val = temp_val * 10 + int(traversal[i] - '0');
+                    continue;
+                }
+                if (chain.size() <= dash_count)
+                {
+                    chain.push_back(new TreeNode(temp_val * 10 + int(traversal[i] - '0')));
+                }
+                else
+                {
+                    chain[dash_count] = new TreeNode(temp_val * 10 + int(traversal[i] - '0'));
+                }
+                temp_val = 0;
+                if (dash_count == 0)
+                {
+                    continue;
+                }
+                if (chain[dash_count - 1]->left) {
+                    chain[dash_count - 1]->right = chain[dash_count];
+                }
+                else {
+                    chain[dash_count - 1]->left = chain[dash_count];
+                }
+                dash_count = 0;
+            }
+        }
+        return chain[0];
+    }
 };
 
 int main()
 {
     Solution sol;
-    std::string input{ "[[0,2,1,0],[4,0,0,3],[1,0,0,4],[0,3,2,0]]" };
-    std::vector<std::vector<int>> grid = Parser::process_vector_vector(input);
-    //long long res = sol.findMaxFish(grid);
+    std::string input{ "1-2--3--4-5--6--7" };
+    TreeNode* res = sol.recoverFromPreorder(input);
 }
